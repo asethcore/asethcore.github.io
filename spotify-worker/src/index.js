5aa9afcaabb4b5a1650b84c5f7c5ab7dcc72f51b
asethcore <westlakehere029@gmail.com>
2026-08-06 14:20:01 +0530

    please gpt i need this

--- spotify-worker/src/index.js
+++ spotify-worker/src/index.js
@@ -1,12 +1,24 @@
+const cors = {
+  "Access-Control-Allow-Origin": "https://asethcore.github.io",
+  "Access-Control-Allow-Methods": "GET, OPTIONS",
+  "Access-Control-Allow-Headers": "*",
+  "Access-Control-Max-Age": "86400",
+};
+
 export default {
   async fetch(request, env) {
-    // Get a fresh Spotify access token
+    if (request.method === "OPTIONS") {
+      return new Response(null, {
+        status: 204,
+        headers: cors,
+      });
+    }
+
     const tokenRes = await fetch("https://accounts.spotify.com/api/token", {
       method: "POST",
       headers: {
         Authorization:
-          "Basic " +
-          btoa(`${env.CLIENT_ID}:${env.CLIENT_SECRET}`),
+          "Basic " + btoa(`${env.CLIENT_ID}:${env.CLIENT_SECRET}`),
         "Content-Type": "application/x-www-form-urlencoded",
       },
       body: new URLSearchParams({
@@ -18,10 +30,15 @@ export default {
     const token = await tokenRes.json();
 
     if (!token.access_token) {
-      return Response.json(token, { status: 500 });
+      return new Response(JSON.stringify(token), {
+        status: 500,
+        headers: {
+          "Content-Type": "application/json",
+          ...cors,
+        },
+      });
     }
 
-    // Ask Spotify what's playing
     const playingRes = await fetch(
       "https://api.spotify.com/v1/me/player/currently-playing",
       {
@@ -32,23 +49,39 @@ export default {
     );
 
     if (playingRes.status === 204) {
-      return Response.json({
-        playing: false,
-        title: "listening",
-        artist: "to the world around him",
-        cover: "",
-        spotify_url: "",
-      });
+      return new Response(
+        JSON.stringify({
+          playing: false,
+          title: "listening",
+          artist: "to the world around him",
+          cover: "",
+          spotify_url: "",
+        }),
+        {
+          headers: {
+            "Content-Type": "application/json",
+            ...cors,
+          },
+        }
+      );
     }
 
     const song = await playingRes.json();
 
-    return Response.json({
-      playing: song.is_playing,
-      title: song.item.name,
-      artist: song.item.artists.map(a => a.name).join(", "),
-      cover: song.item.album.images[0].url,
-      spotify_url: song.item.external_urls.spotify,
-    });
+    return new Response(
+      JSON.stringify({
+        playing: song.is_playing,
+        title: song.item.name,
+        artist: song.item.artists.map((a) => a.name).join(", "),
+        cover: song.item.album.images[0].url,
+        spotify_url: song.item.external_urls.spotify,
+      }),
+      {
+        headers: {
+          "Content-Type": "application/json",
+          ...cors,
+        },
+      }
+    );
   },
 };
